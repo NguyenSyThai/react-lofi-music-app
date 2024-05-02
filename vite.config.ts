615dd7c6f5392d22bbd257feeda92182298149c8
Lacosta <sythai.it@gmail.com>
2024-05-02 20:04:33 +0700

    update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-lofi-music-app/",
   plugins: [react()],
   test: {
     globals: true,
@@ -21,8 +22,4 @@ export default defineConfig({
       },
     },
   },
-  base: "/react-lofi-music-app/",
-  build: {
-    outDir: 'docs'
-  }
 });
